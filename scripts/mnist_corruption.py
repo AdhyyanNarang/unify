abdf3447ae60c2badea3dfb34485b9f69169cf6b
Nathan Cheng <nathancheng@Nathans-MacBook-Pro-5.local>
2019-02-27 18:40:28 -0800

    updated corruption functions

--- scripts/mnist_corruption.py
+++ scripts/mnist_corruption.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 from skimage.filters import gaussian
 import numpy as np
 import matplotlib
-matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
 from cleverhans.attacks import FastGradientMethod
 
@@ -10,14 +9,14 @@ from cleverhans.attacks import FastGradientMethod
 #random epsilon perturbation
 #random pixel blackout/whiteout on mnist
 
-def gaussian_blurring(images, std_dev):
-	return np.array([gaussian(im, std_dev) for im in images])
+def gaussian_blurring(images, std_dev = 2):
+	return np.array([gaussian(im, std_dev, preserve_range = True) for im in images])
 
-def random_perturbation(images, eps):
+def random_perturbation(images, eps = 0.1):
 	return images + np.random.uniform(-eps, eps, images.shape)
 
 #p is probability that a pixel gets corrupted, p_b is probability that a corrupted pixel is blacked_out
-def random_blackout_whiteout(images, p, p_b):
+def random_blackout_whiteout(images, p = 0.2, p_b = 0.5):
 	#0 corresponds to blackout, 2 corresponds to whiteout, 1 means do nothing
 	mask = np.random.choice([0,1,2], images.shape, replace = True, p = [p * p_b, 1 - p, p * (1 - p_b)])
 
@@ -39,45 +38,11 @@ def adversarial_examples(images, model, sess):
 	return adversarial_images
 
 #corrupt n points
-def corrupt_data(original_data, corrupted_data, n, seed = 0):
+def corrupt_data(original_data, n, corrupt_func, seed = 0):
 	np.random.seed(seed)
-	shuffle_indices = list(range(len(original_data)))
-	np.random.shuffle(shuffle_indices)
-	original_data = original_data[shuffle_indices]
-	corrupted_data = corrupted_data[shuffle_indices]
-	return np.vstack((corrupted_data[:n], original_data[n:]))
-
-
-
-
-
-mnist = tf.keras.datasets.mnist
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
-
-temp_set = x_train[:]
-blurred = gaussian_blurring(temp_set, 2)
-perturbed = random_perturbation(temp_set, 0.1)
-black_white = random_blackout_whiteout(temp_set, 0.2, 0.5)
-
-corrupted_set = corrupt_data(temp_set, blurred, 20)
-print(corrupted_set.shape)
-
-plt.imshow(temp_set[0], cmap = "gray")
-plt.show()
-
-plt.imshow(blurred[0], cmap = "gray")
-plt.show()
-
-plt.imshow(perturbed[0], cmap = "gray")
-plt.show()
-
-plt.imshow(black_white[0], cmap = "gray")
-plt.show()
-
-plt.imshow(corrupted_set[19], cmap = "gray")
-plt.show()
-
-plt.imshow(corrupted_set[20], cmap = "gray")
-plt.show()
+	data_copy = np.array(original_data)
+	corrupt_indices = np.random.choice(list(range(len(data_copy))), n, replace = False)
+	corrupted_pts = corrupt_func(data_copy[corrupt_indices])
+	data_copy[corrupt_indices] = corrupted_pts
+	return data_copy
 
